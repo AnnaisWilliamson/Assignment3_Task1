5d850404d6e3841f7873a72bad4a02b197d8c0ce
AnnaisWilliamson <79878620+AnnaisWilliamson@users.noreply.github.com>
2022-05-27 09:57:00 +1000

    Annie parts
    I think i did it right? Added the flex parts to my picker if you wanted quantity to go next to it :)

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import React from "react";
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState} from "react";
+import {SafeAreaView, StyleSheet, View, Text, Image, Picker} from 'react-native';
+
+const [picker1selectedvalue, setPicker1SelectedValue] = useState("val-Potato");
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       {/* 1. Welcome heading  (Annie) */}
+      <View style={styles.row1}>
+      <Text style={styles.heading}> Welcome to VFUS </Text>
+      </View>
 
       {/* 2. Image of your choice (Nishesh) */}
 
       {/* 3. Veggies dropdown (Annie) */}
+      <View style={styles.row3}>
+      <Picker style={styles.picker1}
+      selectedValue={picker1selectedvalue}
+      onValueChange={(itemValue, itemIndex) => setPicker1SelectedValue(itemValue)}>
+        <Picker.Item	label="Veggies" />
+      	<Picker.Item	label="Potato - $5"	value="val-Potato - $5"	/>
+        <Picker.Item	label="Capsicum - $8"	value="val-Capsicum - $8"	/>
+        <Picker.Item	label="Brocolli - $8"	value="val-Brocolli - $8"	/>
+        </Picker>
+      </View>
 
       {/* 4. Fruits dropdown (Nishesh) */}
 
@@ -26,5 +41,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffcc'
+  },
+  heading: {
+    fontSize:20,
+    textAlign: 'center',
+    marginTop: 50
+  },
+    picker1: {
+    flex:2
+  },
+   row3: {
+    marginTop: 20,
+    flexDirection: 'row',
   },
 });
